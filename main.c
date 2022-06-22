e0fd7a6a9047cff41188bc91ea9d6a63c4f59afd
Alex Peterson <alexpeterson0804@gmail.com>
2022-06-21 20:06:33 -0700

    Add mnist dataset

--- main.c
+++ main.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-    int input_size = 3, hidden_size = 2, output_size = 1, num_hidden = 1;
+    int input_size = 728, hidden_size = 32, output_size = 10, num_hidden = 3;
     int only_nodes = 1;
 
     Matrix *input = matrix_build(input_size, 1);
     matrix_randomize(input);
 
     NeuralNetwork *net = network_build(input_size, hidden_size, output_size, num_hidden, input);
-    network_print(net, only_nodes);
+    // network_print(net, only_nodes);
     propagate(net);
 
     printf("\nWow! It didn't crash!\n");
-    network_print(net, only_nodes);
+    // network_print(net, only_nodes);
     return 0;
 }
